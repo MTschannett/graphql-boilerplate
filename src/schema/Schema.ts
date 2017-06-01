cf5a80969193bc0d1a65934925b959af65452e87
Marco Tschannett <marcotschannett@live.de>
2017-06-01 23:43:41 +0200

    removed unused schema resolver logic

--- src/schema/Schema.ts
+++ src/schema/Schema.ts
@@ -1,7 +1,7 @@
 import RootQuery from './RootQuery';
 import {Request} from 'express';
 import RootMutation from './RootMutation';
-import {makeExecutableSchema, addSchemaLevelResolveFunction} from 'graphql-tools';
+import {makeExecutableSchema} from 'graphql-tools';
 import Types from './types/index';
 import MainResolver from './resolvers/MainResolver'
 import * as jwt from 'jsonwebtoken';
@@ -13,16 +13,9 @@ const Schema = `
     }
 `
 
-// TODO maybe add logic to set user object to context
-function authTestFunction(obj:any, args:any, context:any, info:any) {
-    console.log(context.user)
-}
-
 let SchemaType =  makeExecutableSchema({
     typeDefs:[Schema, RootMutation, RootQuery, Types],
     resolvers: MainResolver
 });
-
- addSchemaLevelResolveFunction(SchemaType, authTestFunction)
 
 export default SchemaType;
